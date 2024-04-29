9ad1f967081819ec6d3e63541c570afe77d73c46
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2024-04-29 12:48:15 -0400

    Added camera button

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { uploadFile } from "../cloud/cloud";
 
 function Task({
@@ -11,8 +11,31 @@ function Task({
     username,
 }) {
     const [image, setImage] = useState(null);
+    const [takingPicture, setTakingPicture] = useState(false);
+    const videoRef = useRef(null);
     const onImageChanged = (e) => {
         setImage(e.target.files[0]);
+    };
+    const takePicture = () => {
+        const video = videoRef.current;
+
+        // Create a canvas to draw the video frame
+        const canvas = document.createElement("canvas");
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        const context = canvas.getContext("2d");
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        // Convert the canvas image to a Blob
+        canvas.toBlob((blob) => {
+            setImage(blob);
+            setTakingPicture(false);
+            // // Create a download link for the user
+            // const downloadLink = document.createElement("a");
+            // downloadLink.download = "captured_image.png";
+            // downloadLink.href = URL.createObjectURL(blob);
+            // downloadLink.click();
+        }, "image/png");
     };
     return (
         <div
@@ -31,14 +54,52 @@ function Task({
                 alignItems: "center",
             }}
         >
-            <h1>{name}</h1>
-            <p>{description}</p>
+            <h1 style={{ marginBottom: "0px" }}>{name}</h1>
+            <p style={{ margin: "0px" }}>{description}</p>
             <input
                 style={{ fontSize: "2vh", marginLeft: "20vw" }}
                 type="file"
                 title="Upload Image"
                 accept="image/*"
                 onChange={onImageChanged}
+            />
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <button
+                    style={{ fontSize: "10vh", borderRadius: "1vw" }}
+                    onClick={async () => {
+                        const nowTakingPicture = !takingPicture;
+                        setTakingPicture(!takingPicture);
+                        if (nowTakingPicture) {
+                            const stream =
+                                await navigator.mediaDevices.getUserMedia({
+                                    video: true,
+                                });
+                            videoRef.current.srcObject = stream;
+                        } else {
+                            videoRef.current.srcObject = null;
+                        }
+                    }}
+                >
+                    📷
+                </button>
+                {takingPicture ? (
+                    <button
+                        style={{ fontSize: "10vh", borderRadius: "1vw" }}
+                        onClick={takePicture}
+                    >
+                        📸
+                    </button>
+                ) : null}
+            </div>
+            <video
+                ref={videoRef}
+                style={{
+                    maxWidth: "70vw",
+                    height: "30vh",
+                    display: takingPicture ? "inline" : "none",
+                }}
+                autoPlay
+                muted
             />
             {image ? (
                 <img
